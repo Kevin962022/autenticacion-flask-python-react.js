41836ac35dcdf5b0e06753f31e96a87359288a7e
Kevin Villafuerte Montero <116960904+Kevin962022@users.noreply.github.com>
2024-09-30 23:09:44 +0000

    configuration

--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Footer = () => (
     <footer className="footer mt-4 text-center text-light bg-dark py-3">
-        <p>Copyright &copy; 2024 MageeksApp from Kevin</p>
+        <p>Copyright &copy; 2024 from Kevin</p>
     </footer>
 );
